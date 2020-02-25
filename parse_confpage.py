e0c9518554fed68464e70fae1ea7abbe31cc7623
mardub1635 <pyou.pyou@laposte.net>
2020-02-25 18:16:56 +0100

    extract year create a file confList1.csv

--- parse_confpage.py
+++ parse_confpage.py
@@ -3,20 +3,41 @@ from bs4 import BeautifulSoup
 import spacy
 from spacy import displacy
 from collections import Counter
-
-f=open("confList.csv",'w')
+import dateparser
+f=open("confList1.csv",'w')
 #Parse page
-link="http://www.mt-archive.info/srch/conferences-1.htm"
-html = urlopen(link)
+link="temp1"
+#html = urlopen(link)
+with open(link,'r') as g:
+    html = g.read()
 bs = BeautifulSoup(html, "html.parser")
 pagelines = bs.find_all(['p'])
-
+print(bs.getText)
 #NLP treatment
 nlp = spacy.load("en_core_web_sm")
 
+def getDate(doc):
+    confTime={}
+    for X in doc.ents:
+        if X.label_=='DATE':
+            try:
+                ddata=dateparser.date.DateDataParser().get_date_data(X.text)
+                dtype=ddata['period']
+                dobj=ddata['date_obj']
+                if dtype=="year":
+                    dobj=ddata['date_obj']
+                    confTime["year"]=dobj.year
+                if dtype=="day" and dobj:
+                    confTime["day"]=dobj.day
+                    confTime["month"]=dobj.month
+            except:
+                pass
+    return confTime
+import csv
 
-
-#Look at each line of html file
+writer = csv.writer(f, delimiter=';',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+#Look at each line <p> of html file
 confName=""
 for line in pagelines[5:]:
     #Check if there is a new conference name in bold
@@ -24,8 +45,9 @@ for line in pagelines[5:]:
     if len(boldText)!=0:
         confName=boldText[0].getText()
     doc=nlp(line.getText())
-    print(dir(doc.ents))
-    for X in doc.ents:
-        if X.label_=='DATE':
-            print(X)
-            f.write(confName+";"+X.text)
+    confTime=getDate(doc)
+    if 'year' in confTime:
+
+        writer.writerow([confName, confTime['year'], confTime])
+    else:
+        writer.writerow([confName, 'N/A', confTime])
