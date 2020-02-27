be5fe0268a30bfcbd911c5e0ec7185f57528cd03
mardub1635 <pyou.pyou@laposte.net>
2020-02-27 11:09:20 +0100

    confList1 has links and TOC

--- parse_confpage.py
+++ parse_confpage.py
@@ -4,6 +4,8 @@ import spacy
 from spacy import displacy
 from collections import Counter
 import dateparser
+import re
+from urllib.parse import urlparse
 f=open("confList1.csv",'w')
 #Parse page
 link="temp1"
@@ -28,24 +30,52 @@ def getDate(doc):
                     confTime["year"]=dobj.year
                 if dtype=="day" and dobj:
                     confTime["day"]=dobj.day
-                    confTime["month"]=dobj.month
+                    confTime["month"]=dobj.strftime("%b")
             except:
                 pass
     return confTime
 import csv
+def normaliseSpace(text):
+    if type(text) is list:
+        text=" ".join(text)
+
+    text=re.sub('\s+'," ",text)
+    text=text.strip()
+    return text
+
+def isTOC(a):
+    a=a.lower()
+    return "table" in a and "content" in a
+
+def formatDate(d):
+    return "{}/{}/{}".format(d.setdefault("year", " "), d.setdefault("month", " "), d.setdefault("day", " "))
 
 writer = csv.writer(f, delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
+log=csv.writer(open("log.csv","w"), delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
+writer.writerow(["Conference Title", "Year", "Time","has TOC?","Link"])
 #Look at each line <p> of html file
 confName=""
+
 for line in pagelines[5:]:
     #Check if there is a new conference name in bold
-    boldText=line.find_all(['b'])
-    if len(boldText)!=0:
-        confName=boldText[0].getText()
+    boldText=[b.getText() for b in line.find_all(['b'])]
+    currName=normaliseSpace(boldText)
+    links=line.find_all(['a'])
+    toc = [ a for a in links if isTOC(a.getText())]
+    tocURL=""
+    hasTable="no"
+    if toc:
+        hasTable="yes"
+        tocURL=urlparse(toc[0]['href']).path
+    if currName:
+        confName=currName
+
     doc=nlp(line.getText())
     confTime=getDate(doc)
+    backupYear=re.findall("19\d\d|20|d\d",line.getText())
     if 'year' in confTime:
-
-        writer.writerow([confName, confTime['year'], confTime])
+        writer.writerow([confName, confTime['year'], formatDate(confTime),hasTable,tocURL])
+    elif backupYear:
+        writer.writerow([confName, backupYear,formatDate(confTime),hasTable,tocURL])
     else:
-        writer.writerow([confName, 'N/A', confTime])
+        log.writerow([confName, "N/A", formatDate(confTime),hasTable,tocURL])
